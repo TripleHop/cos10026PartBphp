3c099629166e8d1e677f7fb1f23f015aeb7f0c88
JamesDaWest <pro.zkilz@gmail.com>
2022-05-29 23:34:51 +1000

    added in login enhancement

--- phpenhancements.php
+++ phpenhancements.php
@@ -8,7 +8,19 @@
   <title>MMORPG - PHP Enhancements</title>
 </head>
 <body>
-
+<header><h1>PHP Enhancements</h1></header>
+<section>
+    <h2>PHP Enhancements used in this website</h2>
+    <ul>
+      <li>Supervisor Login</li>
+        <ul>
+            <li>To create a login, we had to create a database with MySQL, inwhich we can add in supervisor usernames and passwords. Data type validation and sanitisation were used to prevent SQL injection attacks.</li>
+            <li><a href="login.php">Click here to see the enhancement</a></li>
+            <li><a href="https://www.simplilearn.com/tutorials/php-tutorial/php-login-form">Source</a> Accessed: 22/5/2022</li>
+        </ul>
+      </br>
+    </ul>
+  </section>
 <?php
 define('PHPENHANCEMENTS', true);
 include_once 'menu.inc';
